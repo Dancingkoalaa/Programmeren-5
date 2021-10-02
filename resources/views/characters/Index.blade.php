61d0bd154b5e05e7d32929153a05b199a97bc58b
Dancingkoalaa <tim2001.tw@gmail.com>
2021-10-02 21:30:43 +0200

    Bootstrap testing

--- resources/views/characters/Index.blade.php
+++ resources/views/characters/Index.blade.php
@@ -5,7 +5,7 @@
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-light border border-dark">
     <div class="container-fluid">
         <div><a class="navbar-brand" href="/">Homepage</a></div>
         <div><a class="navbar-brand" href="/characters">Characters</a></div>
@@ -16,11 +16,16 @@
 </nav>
 <div>
     <h1>{{$title}}</h1>
-    <ul>
+    <div class="container">
+        <div class="row">
         @foreach($characters as $character)
-            <li><a href="characters/{{ $character->id }}">Character: {{$character->Name}} is een {{$character->Race}} en specialiseert in {{$character->proficiency}}</a></li>
+            <div class="col-sm border border-dark">
+                <p href="characters/{{ $character->id }}">Character: {{$character->Name}} is een {{$character->Race}} en specialiseert in {{$character->proficiency}}</p>
+                <button type="button" class="btn btn-outline-dark">Dit gaat ergens naartoe!</button>
+            </div>
         @endforeach
-    </ul>
+        </div>
+    </div>
 </div>
 </body>
 </html>
